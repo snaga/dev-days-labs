f15be85fd25ece0d39d1a12beefe4e001fb98239
Satoshi Nagayasu <snaga@uptime.jp>
2016-12-18 16:11:18 +0900

    動作版 その1 （Azure使わないバージョン）

--- HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
+++ HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
@@ -16,7 +16,71 @@ using System.Runtime.CompilerServices;
 
 namespace DevDaysSpeakers.ViewModel
 {
-    public class SpeakersViewModel
+    public class SpeakersViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        bool busy;
+        public bool IsBusy
+        {
+            get { return busy; }
+            set
+            {
+                busy = value;
+                OnPropertyChanged();
+                //Update the can execute
+                GetSpeakersCommand.ChangeCanExecute();
+            }
+        }
+
+        public ObservableCollection<Speaker> Speakers { get; set; }
+        public Command GetSpeakersCommand { get; set; }
+
+        public SpeakersViewModel()
+        {
+            Speakers = new ObservableCollection<Speaker>();
+
+            GetSpeakersCommand = new Command(
+                async () => await GetSpeakers(),
+                () => !IsBusy);
+        }
+
+        async Task GetSpeakers()
+        {
+            if (IsBusy)
+                return;
+
+            Exception error = null;
+            try
+            {
+                IsBusy = true;
+
+                using (var client = new HttpClient())
+                {
+                    // サーバーから json を取得します
+                    var json = await client.GetStringAsync("http://demo4404797.mockable.io/speakers");
+
+                    var items = JsonConvert.DeserializeObject<List<Speaker>>(json);
+
+                    Speakers.Clear();
+                    foreach (var item in items)
+                        Speakers.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex);
+                error = ex;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+            if (error != null)
+                await Application.Current.MainPage.DisplayAlert("Error!", error.Message, "OK");
+        }
     }
 }
